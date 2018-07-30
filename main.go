ec9a3e8b3e48272e540de4e11e2f7e224453f60d
Cédric Blomart <cblomart@gmail.com>
2018-07-30 13:21:54 +0200

    generate file

--- main.go
+++ main.go
@@ -2,13 +2,40 @@ package main
 
 import (
 	"log"
+	"os"
 	"strings"
+	"text/template"
 
 	git "gopkg.in/src-d/go-git.v4"
 )
 
+const (
+	versionFilename string = "./version.go"
+	versionTemplate string = `
+package main
+
+const(
+	gitCommit = "{{.Commit}}"
+	gitShortCommit = "{{.ShortCommit}}"
+	gitTag= "{{.Tag}}"
+	gitStatus = "{{.Status}}" 
+)
+`
+)
+
+//GitInfo has the informations reported by git-verion
+type GitInfo struct {
+	Commit      string
+	ShortCommit string
+	Tag         string
+	Status      string
+}
+
 func main() {
+	// check current repository
 	path := "./"
+	// create the info object
+	infos := GitInfo{}
 	// open git repo
 	repo, err := git.PlainOpen(path)
 	if err != nil {
@@ -26,16 +53,18 @@ func main() {
 	if err != nil {
 		log.Printf("Could not get last commit from repository '%s'\n", path)
 	}
-	commitname := commit.Hash.String()
+	infos.Commit = commit.Hash.String()
+	infos.ShortCommit = infos.Commit[:7]
 	// print info on the commit
-	log.Printf("Commit: %s\n", commitname)
+	log.Printf("Commit: %s\n", infos.Commit)
+	log.Printf("Short Commit: %s\n", infos.ShortCommit)
 	// get tags
-	tagname := ""
 	tag, err := repo.TagObject(commit.Hash)
 	if err == nil {
-		tagname = strings.Replace(tag.Name, "refs/tags/", "", 1)
+		infos.Tag = strings.Replace(tag.Name, "refs/tags/", "", 1)
 	}
-	log.Printf("Tag: %s\n", tagname)
+	log.Printf("Tag: %s\n", infos.Tag)
+	// get status
 	worktree, err := repo.Worktree()
 	if err != nil {
 		log.Printf("Couldn't get the worktree from repository '%s'", path)
@@ -46,10 +75,24 @@ func main() {
 		log.Printf("Couldn't get the worktree from repository '%s'", path)
 		return
 	}
-	gitstatus := "clean"
+	infos.Status = "clean"
 	if !status.IsClean() {
-		gitstatus = "dirty"
-		log.Println(status.String())
+		infos.Status = "dirty"
 	}
-	log.Printf("Status: %s\n", gitstatus)
+	log.Printf("Status: %s\n", infos.Status)
+	// generate version.go file
+	t := template.New("version.go")
+	_, err = t.Parse(versionTemplate)
+	if err != nil {
+		log.Printf("Could not parse template: %s", err)
+		return
+	}
+	// create file
+	f, err := os.Create(versionFilename)
+	defer f.Close()
+	if err != nil {
+		log.Printf("Could not create file %s: %s\n", versionFilename, err)
+		return
+	}
+	t.Execute(f, infos)
 }
