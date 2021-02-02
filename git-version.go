3209244430e11cbbb136717c3a919e5be761bb5b
Blomart Cédric <cblomart@gmail.com>
2021-02-02 16:26:19 +0100

    use go mod and upgrade git version

--- git-version.go
+++ git-version.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 	"text/template"
 
-	git "gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing"
+	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 )
 
 const (
---
+++ go.mod
@@ -0,0 +1,5 @@
+module cblomart/git-version
+
+go 1.15
+
+require github.com/go-git/go-git/v5 v5.2.0
